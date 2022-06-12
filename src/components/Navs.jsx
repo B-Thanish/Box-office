5795d1c8f75a508b7b7c4a64d57e5e26a0cc5d18
B-Thanish <banothuthanish@gmail.com>
2022-06-12 17:06:05 +0530

    defined theme for styled components and created a few styled components

--- src/components/Navs.jsx
+++ src/components/Navs.jsx
@@ -1,22 +1,33 @@
 /* eslint-disable arrow-body-style */
 /* eslint-disable react/function-component-definition */
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { useLocation } from 'react-router';
+import { NavList, LinkStyled } from './Navs.styled';
 
 const LINKS = [
   { to: '/', text: 'Home' },
   { to: '/starred', text: 'starred' },
 ];
 const Navs = () => {
+  const location = useLocation();
+
+  // eslint-disable-next-line no-console
+  console.log('location', location);
+
   return (
     <div>
-      <ul>
+      <NavList>
         {LINKS.map(item => (
           <li key={item.to}>
-            <Link to={item.to}>{item.text}</Link>
+            <LinkStyled
+              to={item.to}
+              className={item.to === location.pathname ? 'active' : ''}
+            >
+              {item.text}
+            </LinkStyled>
           </li>
         ))}
-      </ul>
+      </NavList>
     </div>
   );
 };
